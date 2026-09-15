ce594c2b1f11102c9e5a61581bc3d7f230b2edd2
mdashrafulalam1986-source <mdashrafulalam1986@gmail.com>
2026-09-16 04:28:54 +0600

    Delete app/src/main/java/com/nova/remote/app/src/main/assets/script.js

--- app/src/main/java/com/nova/remote/app/src/main/assets/script.js
+++
@@ -1,8 +0,0 @@
-// Power button handler connecting JS to Android Native Bridge
-function onPowerButtonClick() {
-    if (typeof AndroidRemote !== "undefined") {
-        AndroidRemote.sendPowerSignal();
-    } else {
-        console.log("Running in browser. IR Blaster trigger disabled.");
-    }
-}
