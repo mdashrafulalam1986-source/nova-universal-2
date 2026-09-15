704de36b63030b50b8419f9be4a164722d441fc9
mdashrafulalam1986-source <mdashrafulalam1986@gmail.com>
2026-09-16 03:08:42 +0600

    Update MainActivity.java

--- app/src/main/java/com/nova/remote/MainActivity.java
+++ app/src/main/java/com/nova/remote/MainActivity.java
@@ -1,11 +1,15 @@
-package com.nova.remote;
+package com.example.novauniversal;
 
 import android.os.Bundle;
+import android.content.Context;
+import android.hardware.ConsumerIrManager;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
-import android.webkit.WebSettings;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
     private WebView webView;
 
     @Override
@@ -15,11 +19,30 @@ public class MainActivity extends AppCompatActivity {
         webView = new WebView(this);
         setContentView(webView);
 
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setDomStorageEnabled(true);
-
-        // Load your WebView UI/JS code here
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.addJavascriptInterface(new RemoteBridge(this), "AndroidRemote");
         webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    public class RemoteBridge {
+        Context mContext;
+        ConsumerIrManager irManager;
+
+        RemoteBridge(Context c) {
+            mContext = c;
+            irManager = (ConsumerIrManager) mContext.getSystemService(Context.CONSUMER_IR_SERVICE);
+        }
+
+        @JavascriptInterface
+        public void sendPowerSignal() {
+            if (irManager != null && irManager.hasIrEmitter()) {
+                int frequency = 38000;
+                int[] pattern = {9000, 4500, 560, 560, 560, 1690, 560, 1690, 560, 560}; 
+                irManager.transmit(frequency, pattern);
+                Toast.makeText(mContext, "Signal Sent!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(mContext, "No IR Blaster found on this device!", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
