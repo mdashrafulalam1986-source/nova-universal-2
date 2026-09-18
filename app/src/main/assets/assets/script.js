f7f3f80fa986c9fc5c7774901c77bce70754ee85
mdashrafulalam1986-source <mdashrafulalam1986@gmail.com>
2026-09-19 01:14:11 +0600

    Update script.js

--- app/src/main/assets/assets/script.js
+++ app/src/main/assets/assets/script.js
@@ -1,18 +1,1 @@
-// Power button trigger
-function onPowerButtonClick() {
-    if (typeof AndroidRemote !== "undefined") {
-        AndroidRemote.sendPowerSignal();
-    } else {
-        console.log("Running in browser. IR Blaster trigger disabled.");
-    }
-}
 
-// General command handler for TV remote buttons
-function onCommand(action) {
-    if (typeof AndroidRemote !== "undefined") {
-        // Send command to native Android bridge
-        AndroidRemote.sendCommand(action);
-    } else {
-        console.log("Action triggered in browser:", action);
-    }
-}
